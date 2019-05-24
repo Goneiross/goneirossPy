6f9695d41d5ff5716d6dbbab3683db53da4ce3c2
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-24 15:01:13 +0200

    Add setup for the package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="goneirossPy",
+    version="0.0.1",
+    author="Goneiross",
+    author_email="guillaume.julien.pellerin@goneiross.org",
+    description="Some usefull tools used in my projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Goneiross/gonPython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
