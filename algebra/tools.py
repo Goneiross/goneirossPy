ffb0980d55f43f2e9412565ccb5e4097c5cf022c
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-04-09 16:57:31 +0200

    Add Gauss function

---
+++ algebra/tools.py
@@ -0,0 +1,16 @@
+def Gauss(A, b):
+    n = len(A)
+    for i in range (0,n):
+        printMatrix(A) 
+        if A[i][i] != 0:
+            p = 1 / A[i][i]
+            for j in range (i,n):
+                A[i][j] *= p
+            b[i]*p
+            for k in range(i+1,n):
+                for j in range(i+1,n):
+                    A[k][j] -= A[k][i]*A[i][j]
+                b[k] -= A[k][i]*b[i]
+                A[k][i]=0 #
+            print(i)
+    return A
