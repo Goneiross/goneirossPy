92e41340f34083386a3bfec8eb9c03acd7e047af
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-04-09 14:15:41 +0200

    Add integration function

--- analysis/tools.py
+++ analysis/tools.py
@@ -32,3 +32,35 @@ def differentiateTwice(f, x0, h):
     """
     df = (f(x0+h)-2*f(x0)+f(x0-h))/(h*h)
     return df
+
+def integrate(f, a, b, N, method):
+    """
+    @param f: function to integrate
+    @param a: initial point
+    @param b: end point
+    @param N: number of intervals for precision
+    @param method: trapeze, rectangle, Simpson, Gauss2
+    @return: integral from a to b of f(x)
+    """
+    h = (b-a)/(N)
+    if method == "trapeze":
+        for i in range(0,n-1):
+            xi = a+i*h
+            Lhf += f(xi)+f(xi+h)
+        Lhf *= h/2
+    elif method == "rectangle":
+        for i in range(0,n-1):
+            xi = a+i*h
+            Lhf += f(xi)+h/2
+        Lhf *= h
+    elif method == "Simpson":
+        for i in range(0,n-1):
+            xi = a+i*h
+            Lhf += f(xi)+4*f(xi+h/2)+f(xi+h)
+        Lhf *= h/6
+    elif method == "Gauss2"
+        for i in range(0,n-1):
+            xi = a+i*h
+            Lhf += f(xi+h*(1/2)*(1-(1/sqrt(3))))+f(xi+h*(1/2)*(1-(1/sqrt(3))))
+        Lhf *= h/2
+    return Lhf
