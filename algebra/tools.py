2521d1b8f569edbfb79694df075dbd8b31c104a9
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-04-10 09:00:23 +0200

    Add Choleski function for tridiagonal positive definite matrices

--- algebra/tools.py
+++ algebra/tools.py
@@ -28,3 +28,20 @@ def Gauss(A, b):
                 A[k][i]=0 
             print(i)
     return A,b
+
+def CholeskiTriDiag(A):
+    """
+    @param A: tridiagonal symetric positive definite matrix
+    @return: L such taht A=LLT
+    """
+    L=[]
+    n = len(A)
+    for i in range(n):
+        L.append([])
+        for j in range(n):
+            L[i].append(0)
+    L[1][1] = sqrt(A[1][1])
+    for i in range(1, n-1):
+        L[i+1][i] = A[i+1][i]/L[i][i]
+        l[i+1][i+1] = sqrt(A[i+1][i+1] - L[i+1][i])
+    return L
